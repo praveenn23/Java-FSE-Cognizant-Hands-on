0ffd7217469c3e097ffa6e170bedc57854933449
praveenn23 <praveenkumarrr04@gmail.com>
2026-07-04 18:47:27 +0530

    Commit

---
+++ react/scorecalculatorapp/src/App.js
@@ -0,0 +1,12 @@
+import {CalculateScore} from './Components/CalculateScore';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name="Steve" School="DNV Public School" total={284} goal={3}/>
+    </div>
+  );
+}
+
+export default App;
