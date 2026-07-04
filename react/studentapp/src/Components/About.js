9cf3feaf98a7b2d58acb5597e0ef4aeceb8a67bd
praveenn23 <praveenkumarrr04@gmail.com>
2026-07-04 17:07:47 +0530

    Commit React

---
+++ react/studentapp/src/Components/About.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export class About extends Component {
+  render() {
+    return (
+      <h3>Welcome to the About Page of Student Management Portal</h3>
+    );
+  }
+}
---
+++ react/studentapp/src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export class Contact extends Component {
+  render() {
+    return (
+      <h3>Welcome to the Contact Page of Student Management Portal</h3>
+    );
+  }
+}
